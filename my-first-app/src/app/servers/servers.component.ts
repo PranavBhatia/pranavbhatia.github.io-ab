b076cd900c3bc3affb8d4fd30852546e392a3284
Pranav Bhatia <bhatiapranav1996@gmail.com>
2018-11-15 17:21:51 -0500

    multiple folders in practice

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', // selects element by element
+  //selector: '[app-servers]', // selects the element by attribute
+  // selector: '.app-servers', // selects the element by class
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
