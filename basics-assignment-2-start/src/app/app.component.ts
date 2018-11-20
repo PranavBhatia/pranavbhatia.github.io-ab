a094674c19598cecbb06fa9e1daf872dc72ace3d
Pranav Bhatia <bhatiapranav1996@gmail.com>
2018-11-20 16:40:40 -0500

    assignemnt 2 complete

---
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  username = '';
+  usernameIsEmpty = true;
+
+  onUpdateUsername(event: Event){
+    if(this.username !== '')
+      this.usernameIsEmpty = false;
+    else
+      this.usernameIsEmpty = true;
+  }
+
+  resetUsername(){
+    this.username = '';
+    this.usernameIsEmpty = true;
+  }
+}
