9b4be05693f30719fbc97525928441df12ea51d3
Pranav Bhatia <bhatiapranav1996@gmail.com>
2018-11-20 22:38:24 -0500

    assignment 3 done

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .count5{
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+
+  displayStatus = true;
+  buttonClickLogArray = [];
+  count = 1;
+  
+  toggleDisplayStatus(){
+    this.displayStatus = !this.displayStatus;
+    this.buttonClickLogArray.push(this.count);
+    this.count++;
+  }
 }
