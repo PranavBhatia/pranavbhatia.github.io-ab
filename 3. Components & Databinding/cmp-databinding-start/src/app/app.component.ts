b1c97351b0e7c17e108740bba08f3906ce357406
PranavBhatia <bhatiapranav1996@gmail.com>
2019-07-22 11:01:29 -0400

    Seeing Lifecycle Hooks in Action

--- 3. Components & Databinding/cmp-databinding-start/src/app/app.component.ts
+++ 3. Components & Databinding/cmp-databinding-start/src/app/app.component.ts
@@ -8,7 +8,15 @@ import {Component} from '@angular/core';
 export class AppComponent {
   serverElements = [{type: 'server', name: 'TestServer', content: 'Let us see if this works!'}];
 
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
+
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
     this.serverElements.push({
       type: 'server',
       name: serverData.serverName,
@@ -16,7 +24,7 @@ export class AppComponent {
     });
   }
 
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
