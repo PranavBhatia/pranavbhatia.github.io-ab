33faca6802d0e1f70a662f46c87ca7136ea8127f
PranavBhatia <bhatiapranav1996@gmail.com>
2019-08-12 16:08:26 -0400

    reformat code

---
+++ 5. Services & Dependency Injection/services-assignment-start/src/app/users.service.ts
@@ -0,0 +1,10 @@
+export class UsersService {
+
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
+
+  onSetToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+}
