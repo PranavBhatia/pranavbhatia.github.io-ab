42011c56fcf218f69a84a09552af3aa1bc80e033
PranavBhatia <bhatiapranav1996@gmail.com>
2019-08-12 16:27:54 -0400

    users service added and in use

--- 5. Services & Dependency Injection/services-assignment-start/src/app/users.service.ts
+++ 5. Services & Dependency Injection/services-assignment-start/src/app/users.service.ts
@@ -7,4 +7,9 @@ export class UsersService {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
   }
+
+  onSetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
 }
