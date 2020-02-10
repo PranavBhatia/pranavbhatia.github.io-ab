79a2716cef79040908504ae4e4d0e6a674c9c20e
PranavBhatia <bhatiapranav1996@gmail.com>
2020-02-10 11:46:44 -0500

    Using Subjects for Error Handling

--- 10. Making Http Requests/http-01-start/src/app/posts.service.ts
+++ 10. Making Http Requests/http-01-start/src/app/posts.service.ts
@@ -2,11 +2,13 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Post} from './post.model';
 import {map} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) {
   }
@@ -18,6 +20,9 @@ export class PostsService {
       .subscribe(
         responseBody => {
           console.log(responseBody);
+        },
+        error => {
+          this.error.next(error.message);
         }
       );
   }
