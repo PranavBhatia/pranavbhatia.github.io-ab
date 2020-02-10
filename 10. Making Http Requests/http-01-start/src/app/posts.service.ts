57b3e2ec5ddf9dd9254065a424b4a58ad3688e53
PranavBhatia <bhatiapranav1996@gmail.com>
2020-02-10 13:21:27 -0500

    Adding Query Params

--- 10. Making Http Requests/http-01-start/src/app/posts.service.ts
+++ 10. Making Http Requests/http-01-start/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Post} from './post.model';
 import {catchError, map} from 'rxjs/operators';
 import {Subject, throwError} from 'rxjs';
@@ -31,11 +31,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
       .get<{ [key: string]: Post }>(
         'https://angular-practice-4e312.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({'Custom-Header': 'Hello'})
+          headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+          params: searchParams
         }
       )
       .pipe(map(responseData => {
